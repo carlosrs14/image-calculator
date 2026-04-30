121b3df8a19642f44c62f15c496b1d451527968b
C <92201805+carlosrs14@users.noreply.github.com>
2026-04-30 12:01:46 -0500

    Refactor/quit sdl2 use qt (#1)
    * aritmethic operations
    
    * cmake
    
    * update readme
    
    * logic operations
    
    * add qt
    
    * quit not implemented options

--- src/aritmethic-operations.cpp
+++ src/aritmethic-operations.cpp
@@ -1,22 +1,84 @@
 #include "aritmethic-operations.hpp"
-#include <vector>
 
-cv::Mat substraction(const cv::Mat& minued, const cv::Mat& subtrahend) {
-    if (minued.empty() || subtrahend.empty() || minued.size() != subtrahend.size()) {
+cv::Mat addition(const cv::Mat& m1, const cv::Mat& m2) {
+    if (m1.empty() || m2.empty() || m1.size() != m2.size() || m1.type() != m2.type()) {
         return cv::Mat();
     }
 
-    std::vector<cv::Mat> m_channels, s_channels;
-    cv::split(minued, m_channels);
-    cv::split(subtrahend, s_channels);
+    cv::Mat out = m1.clone();
+    int channels = m1.channels();
 
-    std::vector<cv::Mat> out_channels;
-    
-    for (size_t i = 0; i < m_channels.size(); i++) {
-        out_channels.push_back(m_channels[i] - s_channels[i]);
+    for (int i = 0; i < m1.rows; i++) {
+        for (int j = 0; j < m1.cols; j++) {
+            for (int c = 0; c < channels; c++) {
+                int val = (int)m1.ptr<uchar>(i)[j * channels + c] + (int)m2.ptr<uchar>(i)[j * channels + c];
+                out.ptr<uchar>(i)[j * channels + c] = (uchar)(val > MAX_PIX ? MAX_PIX : val);
+            }
+        }
+    }
+    return out;
+}
+
+cv::Mat substraction(const cv::Mat& minued, const cv::Mat& subtrahend) {
+    if (minued.empty() || subtrahend.empty() || minued.size() != subtrahend.size() || minued.type() != subtrahend.type()) {
+        return cv::Mat();
     }
 
-    cv::Mat out;
-    cv::merge(out_channels, out);
+    cv::Mat out = minued.clone();
+    int channels = minued.channels();
+
+    for (int i = 0; i < minued.rows; i++) {
+        for (int j = 0; j < minued.cols; j++) {
+            for (int c = 0; c < channels; c++) {
+                int val = (int)minued.ptr<uchar>(i)[j * channels + c] - (int)subtrahend.ptr<uchar>(i)[j * channels + c];
+                out.ptr<uchar>(i)[j * channels + c] = (uchar)(val < 0 ? 0 : val);
+            }
+        }
+    }
+    return out;
+}
+
+cv::Mat multiplication(const cv::Mat& m1, const cv::Mat& m2) {
+    if (m1.empty() || m2.empty() || m1.size() != m2.size() || m1.type() != m2.type()) {
+        return cv::Mat();
+    }
+
+    cv::Mat out = m1.clone();
+    int channels = m1.channels();
+
+    for (int i = 0; i < m1.rows; i++) {
+        for (int j = 0; j < m1.cols; j++) {
+            for (int c = 0; c < channels; c++) {
+                float v1 = m1.ptr<uchar>(i)[j * channels + c] / 255.0f;
+                float v2 = m2.ptr<uchar>(i)[j * channels + c] / 255.0f;
+                out.ptr<uchar>(i)[j * channels + c] = (uchar)(v1 * v2 * 255.0f);
+            }
+        }
+    }
+    return out;
+}
+
+cv::Mat division(const cv::Mat& m1, const cv::Mat& m2) {
+    if (m1.empty() || m2.empty() || m1.size() != m2.size() || m1.type() != m2.type()) {
+        return cv::Mat();
+    }
+
+    cv::Mat out = m1.clone();
+    int channels = m1.channels();
+
+    for (int i = 0; i < m1.rows; i++) {
+        for (int j = 0; j < m1.cols; j++) {
+            for (int c = 0; c < channels; c++) {
+                uchar v2 = m2.ptr<uchar>(i)[j * channels + c];
+                if (v2 == 0) {
+                    out.ptr<uchar>(i)[j * channels + c] = MAX_PIX;
+                } else {
+                    float val = (float)m1.ptr<uchar>(i)[j * channels + c] / (float)v2;
+                    val *= 255.0f;
+                    out.ptr<uchar>(i)[j * channels + c] = (uchar)(val > MAX_PIX ? MAX_PIX : val);
+                }
+            }
+        }
+    }
     return out;
 }
